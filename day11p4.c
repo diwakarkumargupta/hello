977f5bbda1fbf5ab13c3ec3a100f2f30f57927b5
diwakarkumargupta <diwakarkumarg253@gmail.com>
2026-06-11 22:25:07 +0530

    Update day11p4.c

--- day11p4.c
+++ day11p4.c
@@ -1,27 +1,25 @@
-#include<stdio.h>
- int isprime(int n)
- {
-    if(n <=1)
-    return 0;
+#include <stdio.h>
 
-    for(int i=2;i<=n /2;i++)
+long factorial(int n)
+{
+    long fact = 1;
+
+    for(int i = 1; i <= n; i++)
     {
-        if(n%i==0)
-        return 0;
+        fact = fact * i;
     }
-    return 1;
- }
- int main()
- {
+
+    return fact;
+}
+
+int main()
+{
     int num;
 
-    printf("enter a number");
-    scanf("%d",&num);
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-    if(isprime(num))
-    printf("%d is a prime number",num);
-    else
-    printf("%d is not a prime number",num);
+    printf("Factorial of %d = %ld", num, factorial(num));
 
     return 0;
- }
+}
