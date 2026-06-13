8ca057426f9cc93cbb4bd112405aa5705c05844d
diwakarkumargupta <diwakarkumarg253@gmail.com>
2026-06-13 21:38:09 +0530

    day 13 assignment

---
+++ day13p3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int largest = arr[0];
+    int smallest = arr[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] > largest)
+            largest = arr[i];
+
+        if(arr[i] < smallest)
+            smallest = arr[i];
+    }
+
+    printf("Largest = %d\n", largest);
+    printf("Smallest = %d", smallest);
+
+    return 0;
+}
